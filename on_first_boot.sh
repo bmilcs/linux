2a7927d1268aba32fa93ef6e1b2acce353d87cde
bmilcs <bmilcs@yahoo.com>
2020-01-23 17:41:42 -0500

    Create on_first_boot.sh

---
+++ on_first_boot.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# after deploying fresh linux vm, execute the following:
+
+echo system update and upgrade incoming!
+echo ;---------------------------------------------------------------------------------------------------;
+sudo apt-get update && sudo apt dist-upgrade
