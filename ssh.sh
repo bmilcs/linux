cb017d19f7efdcb6d6a3dde428645265773e2873
bmilcs <bmilcs@yahoo.com>
2020-01-24 18:46:13 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- ssh.sh
+++ ssh.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 # after deploying fresh linux vm, execute the following:
 
-echo '---- apt-get & distro update/upgrade ----------------------------------------------------------------'
-echo
-sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade
 echo
 echo '---- setup openssh ----------------------------------------------------------------------------------'
 echo
