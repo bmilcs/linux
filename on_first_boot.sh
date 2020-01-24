7ad9163348dd99218ba34cc7edb3a58ef8024a1b
bmilcs <bmilcs@yahoo.com>
2020-01-24 12:48:54 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- on_first_boot.sh
+++ on_first_boot.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # after deploying fresh linux vm, execute the following:
 
-echo ---- apt-get & distro update/upgrade ----------------------------------------------------------------
+echo '---- apt-get & distro update/upgrade ----------------------------------------------------------------'
 echo
 echo 'testing: sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade'
 echo
-echo ---- setup openssh ----------------------------------------------------------------------------------
+echo '---- setup openssh ----------------------------------------------------------------------------------'
 echo
 sudo su 
 sudo mkdir -p ~/.ssh
@@ -13,7 +13,7 @@ sudo chmod 0700 ~/.ssh
 sudo touch ~/.ssh/authorized_keys
 sudo chmod 0644 ~/.ssh/authorized_keys
 echo
-echo ---- allow root ssh ---------------------------------------------------------------------------------
+echo ---- 'allow root ssh' ---------------------------------------------------------------------------------
 grep -qxF 'PermitRootLogin yes' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 grep -qxF 'PubkeyAuthentication yes' /etc/ssh/sshd_config || echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 grep -qxF 'AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config || echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' >> /etc/ssh/sshd_config
