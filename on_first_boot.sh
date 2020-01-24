8d3eef32eec4380e5f3f204232e0096276c25753
bmilcs <bmilcs@yahoo.com>
2020-01-24 12:30:33 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- on_first_boot.sh
+++ on_first_boot.sh
@@ -3,7 +3,7 @@
 
 echo ---- apt-get & distro update/upgrade ----------------------------------------------------------------
 echo
-sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade
+echo testing: sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade
 echo
 echo ---- setup openssh ----------------------------------------------------------------------------------
 echo
@@ -12,11 +12,13 @@ sudo mkdir -p ~/.ssh
 sudo chmod 0700 ~/.ssh
 sudo touch ~/.ssh/authorized_keys
 sudo chmod 0644 ~/.ssh/authorized_keys
-echo $1 >> ~/.ssh/authorized_keys
 echo
-echo ---- allow root ssh ---------------------------------------------------------------------------------cQueryecho
+echo ---- allow root ssh ---------------------------------------------------------------------------------
 grep -qxF 'PermitRootLogin yes' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 grep -qxF 'PubkeyAuthentication yes' /etc/ssh/sshd_config || echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 grep -qxF 'AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config || echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' >> /etc/ssh/sshd_config
 grep -qxF 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAngRc7vefUjzk2k6noOtBlhAzROXTAxG31mwuMXF2/qM8O795WMBHdPndW/5M7Zxk06waqPDDfsjRNj/Zmhfq62kFdTeUP+4WZlo6SZ6v3xVthhf+WQjEDejsVkRoilZIyyA3dxzbLJZzK0RE/sJ8kbIZ1yb+a8sAI6OSUWvIhhfKyfIilNbATuctXKnZRaQVPKHbsCWhS/BYgpVRJmm6TCtjmEnUZGl1+liio4hvlgaXxsZH5Mi2/+1BcKj/5+OQqq8gM2SNDO/vnfRJLTE9yUrvtvUJUJ6XLWnHVigIjZJK/prdzY/N7dHuUVKVV3NsbdhNgzb4N8hsdzRsiGp7Pw== bmilcs' ~/.ssh/authorized_keys || echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAngRc7vefUjzk2k6noOtBlhAzROXTAxG31mwuMXF2/qM8O795WMBHdPndW/5M7Zxk06waqPDDfsjRNj/Zmhfq62kFdTeUP+4WZlo6SZ6v3xVthhf+WQjEDejsVkRoilZIyyA3dxzbLJZzK0RE/sJ8kbIZ1yb+a8sAI6OSUWvIhhfKyfIilNbATuctXKnZRaQVPKHbsCWhS/BYgpVRJmm6TCtjmEnUZGl1+liio4hvlgaXxsZH5Mi2/+1BcKj/5+OQqq8gM2SNDO/vnfRJLTE9yUrvtvUJUJ6XLWnHVigIjZJK/prdzY/N7dHuUVKVV3NsbdhNgzb4N8hsdzRsiGp7Pw== bmilcs' >> ~/.ssh/authorized_keys
+/etc/rc.d/rc.sshd restart
+exit
+
 
