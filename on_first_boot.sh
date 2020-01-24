7a6b1d09fc1a0fe2a1b8d6471ca5d56b1c10136b
bmilcs <bmilcs@yahoo.com>
2020-01-24 11:55:13 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- on_first_boot.sh
+++ on_first_boot.sh
@@ -5,4 +5,10 @@ echo ---- apt-get & distro update/upgrade --------------------------------------
 echo
 sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade
 echo
-
+echo ---- setup openssh ----------------------------------------------------------------------------------
+echo
+sudo mkdir -p ~/.ssh
+sudo chmod 0700 ~/.ssh
+sudo touch ~/.ssh/authorized_keys
+sudo chmod 0644 ~/.ssh/authorized_keys
+echo $1 $2 > ~/.ssh/authorized_keys
