bc36c235573565dd416942e3f51ed19ec876214f
bmilcs <bmilcs@yahoo.com>
2020-01-24 15:54:52 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- postboot.sh
+++ postboot.sh
@@ -7,7 +7,6 @@ echo 'testing: sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade'
 echo
 echo '---- setup openssh ----------------------------------------------------------------------------------'
 echo
-sudo su 
 sudo mkdir -p ~/.ssh
 sudo chmod 0700 ~/.ssh
 sudo touch ~/.ssh/authorized_keys
@@ -17,6 +16,7 @@ echo ---- 'allow root ssh' -----------------------------------------------------
 grep -qxF 'PermitRootLogin yes' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 grep -qxF 'PubkeyAuthentication yes' /etc/ssh/sshd_config || echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 grep -qxF 'AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config || echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' >> /etc/ssh/sshd_config
+
 /etc/rc.d/rc.sshd restart
 sudo service ssh restart
 exit
