9faf42d93c4dcab8aeea5ee23e1018274d2f2bfb
bmilcs <bmilcs@yahoo.com>
2020-01-24 11:51:22 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- on_first_boot.sh
+++ on_first_boot.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # after deploying fresh linux vm, execute the following:
 
-echo system update and upgrade incoming!
-echo ---------------------------------------------------------------------------------------------------
-sudo apt-get update && sudo apt dist-upgrade
+echo ---- apt-get & distro update/upgrade ----------------------------------------------------------------
+echo
+sudo apt-get update && apt-get upgrade && sudo apt dist-upgrade
+echo
+
