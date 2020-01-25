af380ee4e3032063f179e72f29ca4e1e47c6e0e9
bmilcs <bmilcs@yahoo.com>
2020-01-24 19:03:30 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- ssh.sh
+++ ssh.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 # after deploying fresh linux vm, execute the following:
 
+echo '====================================================================================================='
+echo '====  configuring ssh & rsa key  ===================================================================='
+echo '====================================================================================================='
 echo
-echo '---- setup openssh ----------------------------------------------------------------------------------'
+echo '---- create authorized_keys file --------------------------------------------------------------------'
 echo
 sudo mkdir -p ~/.ssh
 sudo chmod 0700 ~/.ssh
 sudo touch ~/.ssh/authorized_keys
 sudo chmod 0644 ~/.ssh/authorized_keys
 echo
-echo '---- allow root ssh ---------------------------------------------------------------------------------'
+echo '---- configure ssh config ---------------------------------------------------------------------------'
 grep -qxF 'PermitRootLogin yes' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 grep -qxF 'PubkeyAuthentication yes' /etc/ssh/sshd_config || echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 grep -qxF 'AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config || echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' >> /etc/ssh/sshd_config
-/etc/rc.d/rc.sshd restart
 sudo service ssh restart
 exit
