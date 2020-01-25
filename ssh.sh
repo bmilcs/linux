9697a6191226df4ec7d932ba35cede4b8b7ec566
bmilcs <bmilcs@yahoo.com>
2020-01-24 19:10:14 -0500

    p
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- ssh.sh
+++ ssh.sh
@@ -11,14 +11,19 @@ sudo mkdir -p ~/.ssh
 sudo chmod 0700 ~/.ssh
 sudo touch ~/.ssh/authorized_keys
 sudo chmod 0644 ~/.ssh/authorized_keys
+echo ... complete.
 echo
 echo '---- configure ssh config ---------------------------------------------------------------------------'
+echo
 grep -qxF 'PermitRootLogin yes' /etc/ssh/sshd_config || echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
 grep -qxF 'PubkeyAuthentication yes' /etc/ssh/sshd_config || echo 'PubkeyAuthentication yes' >> /etc/ssh/sshd_config
 grep -qxF 'AuthorizedKeysFile %h/.ssh/authorized_keys' /etc/ssh/sshd_config || echo 'AuthorizedKeysFile %h/.ssh/authorized_keys' >> /etc/ssh/sshd_config
 sudo service ssh restart
+echo ... complete.
 echo
 echo '====================================================================================================='
-echo '====  setup complete: paste rsa-key into ~/.ssh/authorized_keys  ===================================='
+echo '====  ssh complete  ================================================================================='
 echo '====================================================================================================='
+echo
+echo '** don't forget to paste rsa-keys!! **'
 exit
