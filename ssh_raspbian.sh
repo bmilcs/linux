7015305995a6710d64d2e64d04756ee736ea2b3a
bmilcs <bmilcs@yahoo.com>
2020-02-08 15:28:52 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- ssh_raspbian.sh
+++ ssh_raspbian.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#  cd /tmp && sudo curl -H 'Accept: application/vnd.github.v3.raw' -O -L https://raw.githubusercontent.com/bmilcs/linux/master/ssh_raspbian.sh && sudo chmod +x *.sh && sudo ./ssh_raspbian.sh
+
+#
+#
+#
 # after deploying fresh linux vm, execute the following:
 echo '====================================================================================================='
 echo '====  configuring ssh & rsa key  ===================================================================='
@@ -6,10 +11,10 @@ echo '==========================================================================
 echo
 echo '---- create authorized_keys file --------------------------------------------------------------------'
 echo
-sudo -u bmilcs mkdir -p ~/.ssh
-sudo -u bmilcs chmod 700 ~/.ssh
-sudo -u bmilcs touch ~/.ssh/authorized_keys
-sudo -u bmilcs chmod 600 ~/.ssh/authorized_keys
+sudo mkdir -p ~/.ssh
+sudo chmod 700 ~/.ssh
+sudo touch ~/.ssh/authorized_keys
+sudo chmod 600 ~/.ssh/authorized_keys
 grep -qxF 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAngRc7vefUjzk2k6noOtBlhAzROXTAxG31mwuMXF2/qM8O795WMBHdPndW/5M7Zxk06waqPDDfsjRNj/Zmhfq62kFdTeUP+4WZlo6SZ6v3xVthhf+WQjEDejsVkRoilZIyyA3dxzbLJZzK0RE/sJ8kbIZ1yb+a8sAI6OSUWvIhhfKyfIilNbATuctXKnZRaQVPKHbsCWhS/BYgpVRJmm6TCtjmEnUZGl1+liio4hvlgaXxsZH5Mi2/+1BcKj/5+OQqq8gM2SNDO/vnfRJLTE9yUrvtvUJUJ6XLWnHVigIjZJK/prdzY/N7dHuUVKVV3NsbdhNgzb4N8hsdzRsiGp7Pw== bmilcs' ~/.ssh/authorized_keys || echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAngRc7vefUjzk2k6noOtBlhAzROXTAxG31mwuMXF2/qM8O795WMBHdPndW/5M7Zxk06waqPDDfsjRNj/Zmhfq62kFdTeUP+4WZlo6SZ6v3xVthhf+WQjEDejsVkRoilZIyyA3dxzbLJZzK0RE/sJ8kbIZ1yb+a8sAI6OSUWvIhhfKyfIilNbATuctXKnZRaQVPKHbsCWhS/BYgpVRJmm6TCtjmEnUZGl1+liio4hvlgaXxsZH5Mi2/+1BcKj/5+OQqq8gM2SNDO/vnfRJLTE9yUrvtvUJUJ6XLWnHVigIjZJK/prdzY/N7dHuUVKVV3NsbdhNgzb4N8hsdzRsiGp7Pw== bmilcs' >> ~/.ssh/authorized_keys
 echo '> authorized_keys imported: root/bmilcs'
 echo
