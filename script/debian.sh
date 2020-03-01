80453cb286a49c9aa5344d3412012f08b8c161f1
bmilcs <bmilcs@yahoo.com>
2020-03-01 14:58:14 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/debian.sh
+++ script/debian.sh
@@ -78,11 +78,11 @@ echo '> install: custom login screen'
       touch /etc/banner
       echo > /etc/banner
       printf "%s" "--- welcome to bmilcs homelab -----------------------" >> /etc/banner
-      printf "\n%s" "            >   host:   " >> /etc/banner
+      printf "\n%s" "         > host:   " >> /etc/banner
       echo $HOSTNAME >> /etc/banner
       ipp="ip a | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*/\2/p'"
       eval ip=\$\($ipp\)
-      printf "%s" "            >   ip:     " >> /etc/banner
+      printf "%s" "         > ip:     " >> /etc/banner
       echo $ip >> /etc/banner
       printf "%s\n\n" "-----------------------------------------------------" >> /etc/banner
       sudo /etc/init.d/ssh restart
