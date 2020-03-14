a4a0eaa95ac8551916292f6197917fbd523bd35d
bmilcs <bmilcs@yahoo.com>
2020-03-14 18:12:44 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/ishares.sh
+++ script/ishares.sh
@@ -2,11 +2,9 @@
 
 cd /
 mkdir -p /nas /nas/media/movies /nas/media/tv /nas/media/sports /nas/media/podcasts /nas/media/music /nas/media/audiobooks /nas/media/books /nas/dl /nas/docker
-
 echo '====================================================================================================='
 echo '====  unraid media  ================================================================================='
 echo '====================================================================================================='
-
 sudo grep -qxF '#bmilcs unraid file shares' /etc/fstab || sudo echo '#bmilcs unraid file shares' >> /etc/fstab
 sudo grep -qxF 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
 sudo grep -qxF 'unraid:/mnt/user/storage/video/tv       /nas/media/tv nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/video/tv       /nas/media/tv nfs     auto,defaults,nofail 0 0' >> /etc/fstab
@@ -15,27 +13,20 @@ sudo grep -qxF 'unraid:/mnt/user/storage/audio/podcasts   /nas/media/podcasts   
 sudo grep -qxF 'unraid:/mnt/user/storage/audio/music   /nas/media/music     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/audio/music   /nas/media/music     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
 sudo grep -qxF 'unraid:/mnt/user/storage/audio/audiobooks   /nas/media/audiobooks     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/audio/audiobooks   /nas/media/audiobooks     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
 sudo grep -qxF 'unraid:/mnt/user/storage/literature/books   /nas/media/books     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/literature/books   /nas/media/books     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
-
 echo '----  download location  ------------------------------------------------------------------------------'
 sudo grep -qxF 'unraid:/mnt/user/dl   /nas/dl     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/dl   /nas/dl     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
 # esxi/docker > /nas/docker/
 echo '----  backup  -----------------------------------------------------------------------------------------'
-sudo grep -qxF 'unraid:/mnt/user/storage/backup/esxi/docker   /nas/backup     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/backup/esxi/docker   /nas/backup     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
-
+sudo grep -qxF 'unraid:/mnt/user/esxi/backup/docker   /nas/backup     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/backup/esxi/docker   /nas/backup     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
 echo '====================================================================================================='
 echo '====  network shares installed  ====================================================================='
 echo '====================================================================================================='
-
 cat /etc/fstab
-
 echo
 echo
 echo '----  attempting to mount new shares  -----------------------------------------------------------------'
 echo
-
-
 mount -av
-
 echo
 echo '----  done  -------------------------------------------------------------------------------------------'
 echo
