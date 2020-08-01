a69c959482db5e83577abea57b3b4c87e4ee3305
bmilcs <bmilcs@yahoo.com>
2020-08-01 13:10:53 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 #-----------------------------------------------------------------------------------------------------
 # symlink all dotfiles from repo
-for file in $(find . -maxdepth 1 -path "~/_bmilcs/dotfiles/.*" -type f -printf "%f\n" ); do
+bmDir=$(pwd)
+cd ~/_bmilcs
+for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
     if [ -e ~/$file ]; then
         mv -f ~/$file{,.dtbak}
     fi
@@ -9,10 +11,10 @@ for file in $(find . -maxdepth 1 -path "~/_bmilcs/dotfiles/.*" -type f -printf "
 done
 # add source loop for new files
 sudo grep -qxF '#initialize bmilcs dot files' ~/.bashrc || printf "\n\n#initialize bmilcs dot files\nfor bmfile in ~/.bm*\ndo\n\tsource \"\$bmfile\"\ndone\n" >> ~/.bashrc
-
+cd $bmDir
 #clear
 echo
 echo '====  bmilcs dotfiles refreshed  ===================================================================='
 echo
-cd -
+
 
