be8f17dce4aff2b89a0ba0ab22013bd17fa0a8af
bmilcs <bmilcs@yahoo.com>
2020-09-11 20:35:26 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/debian.sh
+++ script/debian.sh
@@ -60,6 +60,8 @@ echo '... done.' && echo
 # custom login notice
 echo '> custom ssh login msg'
 # remove stock crap
+sed -i '/^HUSHLOGIN_FILE/c\#HUSHLOGIN_FILE .hushlogins' /etc/login.defs
+sed -i '/#HUSHLOGIN_FILE /etc/hushlogins/c\HUSHLOGIN_FILE /etc/hushlogins' /etc/login.defs
 sed -i '/PrintMotd/c\PrintMotd no' /etc/ssh/sshd_config
 sed -i '/PrintLastLog/c\PrintLastLog no' /etc/ssh/sshd_config
 sed -i '/session    optional     pam_motd.so/c\' /etc/pam.d/sshd
