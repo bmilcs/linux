81a1357d47f649abe409ca6442fa9b1c40c5cbcc
bmilcs <bmilcs@yahoo.com>
2020-09-12 19:32:44 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,7 +4,7 @@
 bmDir=$(pwd)
 
 # symlink all dotfiles from repo
-cd /etc/bmilcs/dotfiles
+# cd /dotfiles
 # for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
 #     if [ -e ~/$file ]; then
 #         mv -f ~/$file{,.dtbak}
@@ -12,7 +12,7 @@ cd /etc/bmilcs/dotfiles
 #     ln -s $PWD/$file ~/$file
 # done
 # add source loop for new files
-sudo grep -qxF '#initialize bmilcs dot files' /etc/bash.bashrc || printf "\n#initialize bmilcs dot files\nfor bmfile in /etc/bmilcs/dotfiles/.bm*\ndo\n\tsource \"\$bmfile\"\ndone" >> /etc/bash.bashrc
+sudo grep -qxF '#initialize bmilcs dot files' /etc/bash.bashrc || printf "\n#initialize bmilcs dot files\nfor bmfile in ~/.bm/dotfiles/.bm*\ndo\n\tsource \"\$bmfile\"\ndone" >> /etc/bash.bashrc
 
 # dircolors import (arctic)
 
