01b2fe70a98773c7cac7232cb940660fd207c61e
bmilcs <bmilcs@yahoo.com>
2020-09-12 20:19:16 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/debian.sh
+++ script/debian.sh
@@ -69,7 +69,7 @@ echo '... done.' && echo
 echo '> custom ssh login msg'
 # remove stock crap
 sed -i '/^HUSHLOGIN_FILE \.hushlogins/c\#HUSHLOGIN_FILE .hushlogins' /etc/login.defs
-sed -i '/#HUSHLOGIN_FILE \/etc/c\HUSHLOGIN_FILE /etc/hushlogins' /etc/login.defs
+sed -i '/HUSHLOGIN_FILE \//c\HUSHLOGIN_FILE /etc/hushlogins' /etc/login.defs
 echo /bin/bash | sudo tee -a /etc/hushlogins
 sed -i '/PrintMotd/c\PrintMotd no' /etc/ssh/sshd_config
 sed -i '/PrintLastLog/c\PrintLastLog no' /etc/ssh/sshd_config
