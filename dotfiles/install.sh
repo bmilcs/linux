ae0d3c5ebe0f5fdc82d1c35fa026ddb104dcb4ed
bmilcs <bmilcs@yahoo.com>
2020-09-12 21:02:22 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -16,7 +16,7 @@ done
 sudo grep -qxF '# initialize bmilcs dot files' ~/.bashrc || printf "\n\n#=====================================================================================================\n#====  bmilcs customizations  ========================================================================\n#=====================================================================================================\n\n# initialize bmilcs dot files\nfor bmfile in ~/.bm_*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
 
 # dircolors import (arctic)
-sudo grep -qxF '# custom dir colors' ~/.bashrc || printf "\n\n# custom dir colors\neval \"\$(dircolors ~/.bm/dotfiles/.dir_colors)\"" >> ~/.bashrc
+sudo grep -qxF '# custom dir colors' ~/.bashrc || printf "\n\n# custom dir colors\neval \"\$(dircolors ~/.dir_colors)\"" >> ~/.bashrc
 
 # ls - group dot files
 sudo grep -qxF '# ls > group dotfiles together' ~/.bashrc || printf "\n\n# ls > group dotfiles together\nexport LC_COLLATE=\"C\"" >> ~/.bashrc
