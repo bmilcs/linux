8adcfa339e467dcc33aceb2b854aaef50a7fbf9b
bmilcs <bmilcs@yahoo.com>
2020-09-12 20:45:38 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -4,15 +4,15 @@
 bmDir=$(pwd)
 
 # symlink all dotfiles from repo
-# cd /dotfiles
-# for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
-#     if [ -e ~/$file ]; then
-#         mv -f ~/$file{,.dtbak}
-#     fi
-#     ln -s $PWD/$file ~/$file
-# done
+cd /dotfiles
+for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
+    if [ -e ~/$file ]; then
+        mv -f ~/$file{,.dtbak}
+    fi
+    ln -s $PWD/$file ~/$file
+done
 # add source loop for new files
-sudo grep -qxF '# initialize bmilcs dot files' ~/.bashrc || printf "\n\n#=====================================================================================================\n#====  bmilcs customizations  ========================================================================\n#=====================================================================================================\n\n# initialize bmilcs dot files\nfor bmfile in ~/.bm/dotfiles/.*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
+sudo grep -qxF '# initialize bmilcs dot files' ~/.bashrc || printf "\n\n#=====================================================================================================\n#====  bmilcs customizations  ========================================================================\n#=====================================================================================================\n\n# initialize bmilcs dot files\nfor bmfile in ~/.bm*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
 
 # dircolors import (arctic)
 sudo grep -qxF '# custom dir colors' ~/.bashrc || printf "\n\n# custom dir colors\neval \"\$(dircolors ~/.bm/dotfiles/.dir_colors)\"" >> ~/.bashrc
