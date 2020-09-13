6232891efcdc6f388fd055a2642153bba21b7b03
bmilcs <bmilcs@yahoo.com>
2020-09-12 20:29:17 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -12,14 +12,16 @@ bmDir=$(pwd)
 #     ln -s $PWD/$file ~/$file
 # done
 # add source loop for new files
-sudo grep -qxF '#initialize bmilcs dot files' ~/.bashrc || printf "\n#initialize bmilcs dot files\nfor bmfile in ~/.bm/dotfiles/.bm*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
+""
+
+sudo grep -qxF '#initialize bmilcs dot files' ~/.bashrc || printf "=====================================================================================================\n====  bmilcs customizations  ========================================================================\n=====================================================================================================\n\n#initialize bmilcs dot files\nfor bmfile in ~/.bm/dotfiles/.bm*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
 
 # dircolors import (arctic)
 sudo grep -qxF '#custom dir colors' ~/.bashrc || printf "\n\n#custom dir colors\neval \"\$(dircolors ~/.bm/dotfiles/.dir_colors)\"" >> ~/.bashrc
 
-
 # ls - group dot files
 sudo grep -qxF '#group dotfiles together ls command' ~/.bashrc || printf "\n\n#group dotfiles together ls command\nexport LC_COLLATE=\"C\"" >> ~/.bashrc
+
 #clear
 echo
 echo '====  bmilcs dotfiles refreshed  ===================================================================='
