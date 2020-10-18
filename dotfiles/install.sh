89e8b72c652eaa3d6b578bd79c8f3a2d2356ef85
bmilcs <bmilcs@yahoo.com>
2020-10-18 13:30:10 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
+
 # symlink all dotfiles from repo
+
 cd ~/.bmilcs/dotfiles
+
 case $PWD/ in   */.bmilcs/dotfiles*) echo;;   *) exit 1;; esac
+
 for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
     if [ -e ~/$file ]; then
         mv -f ~/$file{,.old}
@@ -10,12 +14,17 @@ for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
 done
 # add source loop for new files
 sudo grep -qxF '# initialize bmilcs dot files' ~/.bashrc || printf "\n\n#=====================================================================================================\n#====  bmilcs customizations  ========================================================================\n#=====================================================================================================\n\n# initialize bmilcs dot files\nfor bmfile in ~/.bm_*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
+
 sed -i '/for bmfile in/c\for bmfile in ~/.bm_*' ~/.bashrc
+
 # dircolors import (arctic)
 sudo grep -qxF '# custom dir colors' ~/.bashrc || printf "\n\n# custom dir colors\neval \"\$(dircolors ~/.dir_colors)\"" >> ~/.bashrc
+
 sed -i '/eval \"\$(dircolors/c\eval "$(dircolors ~/.dir_colors)"' ~/.bashrc
+
 # ls - group dot files
 sudo grep -qxF '# ls > group dotfiles together' ~/.bashrc || printf "\n\n# ls > group dotfiles together\nexport LC_COLLATE=\"C\"" >> ~/.bashrc
+
 #clear
 echo
 echo '====  bmilcs dotfiles refreshed  ===================================================================='
--- script/debian.sh
+++ script/debian.sh
@@ -40,8 +40,8 @@ fi
 sed -i '/PermitRootLogin/c\PermitRootLogin no' /etc/ssh/sshd_config
 
 # install essentials
-echo && echo "----  apt install: sudo | open-vm-tools | nfs-common | dnsutils  --------------------------------------" && echo
-apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install sudo open-vm-tools nfs-common dnsutils -y && echo && echo #unattended-upgrades apt-listchanges 
+echo && echo "----  apt install: sudo | open-vm-tools | nfs-common | dnsutils | ncdu | htop -----------------------------------" && echo
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y && apt-get install sudo ncdu htop open-vm-tools nfs-common dnsutils -y && echo && echo #unattended-upgrades apt-listchanges 
 
 # remove grub pause on boot
 echo '> removing grub pause' && echo
