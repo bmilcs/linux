989cc3a03c67a5e160cc397cdd6fa2a4a5de4786
bmilcs <bmilcs@yahoo.com>
2020-10-22 18:07:37 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,33 +1,23 @@
 #!/bin/bash
-
 # symlink all dotfiles from repo
-echo -e "${RED}> installing dot files"
-
 cd ~/.bmilcs/dotfiles
-
 case $PWD/ in   */.bmilcs/dotfiles*) echo;;   *) exit 1;; esac
-
 for file in $(find . -maxdepth 1 -name ".*" -type f -printf "%f\n" ); do
     if [ -e ~/$file ]; then
         mv -f ~/$file{,.old}
     fi
     ln -s $PWD/$file ~/$file
 done
-
 # add source loop for new files
 sudo grep -qxF '# initialize bmilcs dot files' ~/.bashrc || printf "\n\n#=====================================================================================================\n#====  bmilcs customizations  ========================================================================\n#=====================================================================================================\n\n# initialize bmilcs dot files\nfor bmfile in ~/.bm_*\ndo\n\tsource \"\$bmfile\"\ndone" >> ~/.bashrc
-
-sed -i '/for bmfile in/c\for bmfile in ~/.bm_*' ~/.bashrc
-
-# dircolors import (arctic)
+# # fix issues w/ dotfiles
+# sed -i '/for bmfile in/c\for bmfile in ~/.bm_*' ~/.bashrc
+# dircolors import (nord)
 sudo grep -qxF '# custom dir colors' ~/.bashrc || printf "\n\n# custom dir colors\neval \"\$(dircolors ~/.dir_colors)\"" >> ~/.bashrc
-
 sed -i '/eval \"\$(dircolors/c\eval "$(dircolors ~/.dir_colors)"' ~/.bashrc
-
 # ls - group dot files
 sudo grep -qxF '# ls > group dotfiles together' ~/.bashrc || printf "\n\n# ls > group dotfiles together\nexport LC_COLLATE=\"C\"" >> ~/.bashrc
-
-#clear
+# let the ppl know
 echo -e "====  ${RED}bmilcs dotfiles refreshed${NC}  ===================================================================="
 # crontab -l | grep -qF '* * up' || (crontab -l >> ~/cronny && echo '30 1 * * * up' >> ~/cronny && crontab ~/cronny && rm ~/cronny)
 # echo && echo "----  cronjob: auto-update installed  -----------------------------------------------------------------" && echo
