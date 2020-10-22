7de0c8a88dd4351f700e2c2ee4f1a16202764967
bmilcs <bmilcs@yahoo.com>
2020-10-22 19:51:19 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+NC='\033[0m'
+BLK='\033[30m'
+BBLK='\033[1;30m'
+RED='\033[31m'
+BRED='\033[1;31m'
+GRN='\033[32m'
+BGRN='\033[1;32m'
+YLW='\033[33m'
+BYLW='\033[1;33m'
+BLU='\033[34m'
+BBLU='\033[1;34m'
+PUR='\033[35m'
+BPUR='\033[1;35m'
+CYN='\033[36m'
+BCYN='\033[1;36m'
+WHT='\033[37m'
+BWHT='\033[1;37m'
+
 # symlink all dotfiles from repo
 cd ~/.bmilcs/dotfiles
 case $PWD/ in   */.bmilcs/dotfiles*) echo;;   *) exit 1;; esac
