7a774fb0785d581c2798fd7a96573f2b248f21b7
bmilcs <bmilcs@yahoo.com>
2020-10-25 14:40:33 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/ufw-unifi.sh
+++ script/ufw-unifi.sh
@@ -2,8 +2,8 @@
 #----  unifi firewall set: unifi  ----------------------------------------------------------------------
 
 ports="3478/udp 5514/udp 8080/tcp 8443/tcp 8880/tcp 8843/tcp 6789/tcp 27117/tcp 5656-5699/udp 10001/udp 1900/udp"
-for i in $ports
+for p in $ports
 do
-	echo "sudo ufw allow " $ports
-	sudo ufw allow $ports
+	echo "sudo ufw allow " $p
+	sudo ufw allow $p
 done
