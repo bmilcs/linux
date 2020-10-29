84048920acfc153a3a1caae37b522649350af9bb
bmilcs <bmilcs@yahoo.com>
2020-10-29 17:01:24 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -2,11 +2,11 @@
 #		BMILCS: backup
 #			featuring "rsnapshot"
 #=====================================================================================================
-NC='\033[0m' ; BLK='\033[30m' ; BBLK='\033[1;30m' ; RED='\033[31m' ; BRED='\033[1;31m' ; GRN='\033[32m' ; BGRN='\033[1;32m' ; YLW='\033[33m' ; BYLW='\033[1;33m' ; BLU='\033[34m' ; BBLU='\033[1;34m' ; PUR='\033[35m' ; BPUR='\033[1;35m' ; CYN='\033[36m' ; BCYN='\033[1;36m' ; WHT='\033[37m' ; BWHT='\033[1;37m'
+NC='\033[0m' ; BLK='\033[30m' ; RED='\033[31m' ; GRN='\033[32m' ; YLW='\033[33m' ; BLU='\033[34m' ; PUR='\033[35m' ; CYN='\033[36m' ; WHT='\033[37m' ; B=$(tput bold) ; N=$(tput sgr0)
 echo && echo "----  bmilcs: backup script  --------------------------------------------------------------------------" && echo
 echo '> root check'
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}ERROR: This script must be run as root" 
+   echo -e "${RED}${B}ERROR${N}:${NC} This script must be run as root" 
    exit 1
 else 
 	echo '... done.' &&	echo
