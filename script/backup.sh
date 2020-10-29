96109df5e22becbc4d53e07fb55ad67c8d1a1a37
bmilcs <bmilcs@yahoo.com>
2020-10-29 18:12:10 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -2,17 +2,17 @@
 #		BMILCS: backup
 #			featuring "rsnapshot"
 #=====================================================================================================
-NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'
-BLK=${NC}'\033[30m' ; RED=${NC}'\033[31m' ; GRN=${NC}'\033[32m' ; YLW=${NC}'\033[33m' ; BLU=${NC}'\033[34m' ; PUR=${NC}'\033[35m' ; CYN=${NC}'\033[36m' ; WHT=${NC}'\033[37m'
+NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
 
-echo -e "${BLU}${DIM}----  ${BLU}${B}bmilcs.backup${BLU} started${DIM}  --------------------------------------------------------------------------\n"
-echo '> root check'
+echo -e "${BLU}${DIM}----  ${BLU}${B}bmilcs-backup${GRN} started${BLU}${DIM}  ------------------------------------------------------------------------"
+echo -e "${DIM}$ ${BLU}root check${NC}"
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}${B}ERROR:${RED} ${BLINK}This script must be run as root.${NC}" 
+	echo -e "${BLU}${DIM}----  ${RED}${B}error:${YLW} root access required  ${BLU}${DIM}----------------------------------------------------\n"
    exit 1
 else 
-	echo -e "${RED}${B}ERROR:${RED} ${BLINK}This script must be run as root.${NC}" 
-	echo -e "${GRN}${B}ROOT ${GRN}permission granted.${NC}" 
+	echo -e "${DIM}  ${GRN}[✓]${NC}" 
+	
 fi
+
 
 # sudo grep -qxF 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
