2e4a98cd55f4936601618d4df12d953c2a8f0b49
bmilcs <bmilcs@yahoo.com>
2020-10-29 17:19:43 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -9,7 +9,7 @@ ITAL='\033[3m'
 UL='\033[4m'
 BLINK='\033[5m'
 INV='\033[7m'
-BLK='\033[30m' ; RED='\033[31m' ; GRN='\033[32m' ; YLW='\033[33m' ; BLU='\033[34m' ; PUR='\033[35m' ; CYN=${NC}'\033[36m' ; WHT='\033[37m'
+BLK=${NC}'\033[30m' ; RED=${NC}'\033[31m' ; GRN=${NC}'\033[32m' ; YLW=${NC}'\033[33m' ; BLU=${NC}'\033[34m' ; PUR=${NC}'\033[35m' ; CYN=${NC}'\033[36m' ; WHT=${NC}'\033[37m'
 
 
 
@@ -17,9 +17,10 @@ BLK='\033[30m' ; RED='\033[31m' ; GRN='\033[32m' ; YLW='\033[33m' ; BLU='\033[34
 echo -e "\n${DIM}${B}----  ${CYN}bmilcs: backup script  --------------------------------------------------------------------------\n"
 echo '> root check'
 if [[ $EUID -ne 0 ]]; then
-   echo -e "${RED}${B}ERROR:${NC} ${BLINK}This script must be run as root.${NC}" 
+   echo -e "${RED}${B}ERROR:${RED} ${BLINK}This script must be run as root.${NC}" 
    exit 1
 else 
+	echo -e "${RED}${B}ERROR:${RED} ${BLINK}This script must be run as root.${NC}" 
 	echo -e "${GRN}${B}ROOT${NC} ${GRN}permission granted.${NC}" 
 fi
 
