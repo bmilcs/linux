f25683e3451a4593a7c9ec2a5be294980e567442
bmilcs <bmilcs@yahoo.com>
2020-10-29 17:11:13 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -20,7 +20,7 @@ if [[ $EUID -ne 0 ]]; then
    echo -e "${RED}${B}ERROR:${N} ${ITAL}This script must be run as root.${NC}" 
    exit 1
 else 
-	echo '... done.' &&	echo
+	echo -e "${GRN}${B}ROOT${N} ${ITAL}permission granted.${NC}" 
 fi
 
 # sudo grep -qxF 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' /etc/fstab || sudo echo 'unraid:/mnt/user/storage/video/movies   /nas/media/movies     nfs     auto,defaults,nofail 0 0' >> /etc/fstab
