7b992d4016c929e15af274ec717659a27a5b5851
bmilcs <bmilcs@yahoo.com>
2020-10-30 12:53:55 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -17,19 +17,32 @@ else
         echo -e "  ${GRN}[√] done.${NC}\n"
 fi
 #ENSURE FOLDER IS CREATED ON FREENAS
-echo -e "${PUR}• ${BLU}ssh into FREENAS & create folder: ${PUR}/mnt/bm/data/backup/${HOSTNAME} ${NC}"
-# echo -e "${DIM}> ${NC}${BLU}ssh into FREENAS ${GRN}\"../backup/${HOSTNAME}\" ${NC}${BLU}\n  type \"${HOSTNAME}\" once done! Make sure!"
+echo -e "${PUR}• ${BLU}ssh into FREENAS & create folder: ${PUR}/mnt/bm/data/backup/${YLW}${HOSTNAME} ${NC}"
 read -e varUSER
-
 if [ "$varUSER" != "$HOSTNAME" ]; then
-echo -e "${BLU}${DIM}----  ${RED}${B}exiting${YLW}: incorrect host name provided  ${BLU}${DIM}-----------------------------------------------------\n"
-		exit 1
+        echo -e "${BLU}${DIM}----  ${RED}${B}exiting${YLW}: incorrect host name provided  ${BLU}${DIM}-----------------------------------------------------\n"
+	exit 1
 else
         echo -e "${DIM}  ${GRN}[✓]${NC}" 
 fi
+# CHECK IF BACKUP USER GROUP EXISTS
+echo -e "${PUR}• ${BLU}checking for bmbak group ${NC}\n"
+grep bmbak /etc/group 2>&1>/dev/null
+if [$? != 0]
+then
+        echo -e "${RED}  [X] ${B}error     ${YLW}        bmbak is missing\n${NC}\n"
+        # CREATE BMBAK GROUP
+        echo -e "${PUR}• ${BLU}        creating bmbak (1999) ${NC}\n"
+        groupadd -g 1999 bmbak
+        getent group | grep bmbak
+        # useradd -g $USERGROUP -d /home/$USERNAME -s /bin/bash -m $USERNAME
+fi
 
-
+# CREATE /NFS/HOST FOLDER
 mkdir -p /nfs/${HOSTNAME}
-FST='10.9.9.100:/mnt/bm/data/backup/test/'${HOSTNAME}'   /nas/media/movies     nfs     auto,defaults,nofail 0 0'
+# ASSIGN PERMISSIONS
+
+# ADD FSTAB MOUNT
+FST='10.9.9.100:/mnt/bm/data/backup/test/'${HOSTNAME}'   /nfs/'$HOSTNAME'     nfs     auto,defaults,nofail 0 0'
 sudo grep -qxF "${FST}" /etc/fstab || sudo echo "${FST}" >> /etc/fstab
 
