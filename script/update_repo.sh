96c5f664951c7a9725650cf47d0abd7fd3787f3d
bmilcs <bmilcs@yahoo.com>
2020-10-30 16:05:21 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/update_repo.sh
+++ script/update_repo.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
 echo -e "${PUR}• ${BLU}bmilcs.git update ${NC}"
-echo -e "${PUR}• ${BLU}YES REALLY UPDATING${NC}\n"
 cd ~ && rm -rf ~/.bm_* ~/.dir_colors ~/.bm ~/.bmilcs ~/_bmilcs && git clone --quiet https://github.com/bmilcs/linux.git ~/.bmilcs && chmod -R +x ~/.bmilcs && (cd ~/.bmilcs/dotfiles ; ./install.sh) && source ~/.bashrc
 echo -e "  ${GRN}[√] done.${NC}\n"
 # echo -e "${BLU}${DIM}----  ${BLU}bmilcs.git update: ${GRN}completed  ${BLU}${DIM}---------------------------------------------------------------\n${NC}"    
