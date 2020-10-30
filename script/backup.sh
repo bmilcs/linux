0fdf2e399512cff1ab28c4d04403f433dacead83
bmilcs <bmilcs@yahoo.com>
2020-10-29 20:18:44 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -5,13 +5,13 @@
 #=====================================================================================================
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
 # ROOT CHECK
-echo -e "${BLU}${DIM}----  ${BLU}${B}bmilcs-backup${GRN} started${BLU}${DIM}  ------------------------------------------------------------------------"
+echo -e "${BLU}${DIM}----  ${BLU}${B}bmilcs-backup${GRN} started${BLU}${DIM}  ------------------------------------------------------------------------\n"
 echo -e "${PUR}• ${BLU}root check ${NC}"
 if [[ $EUID -ne 0 ]]; then
-echo -e "${RED}  [X] ${B}error     ${YLW}root access required\n${NC}"
+echo -e "${RED}  [X] ${B}error     ${YLW}root access required\n${NC}\n"
    exit 1
 else 
-echo -e "  ${GRN}[√] done.${NC}"
+echo -e "  ${GRN}[√] done.${NC}\n"
 fi
 
 
