79c898ef1818cd7568efd6f0ebef514c1f294a1f
bmilcs <bmilcs@yahoo.com>
2020-10-30 13:01:29 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -17,13 +17,13 @@ else
         echo -e "  ${GRN}[√] done.${NC}\n"
 fi
 #ENSURE FOLDER IS CREATED ON FREENAS
-echo -e "${PUR}• ${BLU}ssh into FREENAS & create folder: ${PUR}/mnt/bm/data/backup/${YLW}${HOSTNAME} ${NC}"
+echo -e "${PUR}• ${BLU}ssh into FREENAS & create folder: ${PUR}/mnt/bm/data/backup/${YLW}${HOSTNAME} ${NC}\n"
 read -e varUSER
 if [ "$varUSER" != "$HOSTNAME" ]; then
         echo -e "${BLU}${DIM}----  ${RED}${B}exiting${YLW}: incorrect host name provided  ${BLU}${DIM}-----------------------------------------------------\n"
 	exit 1
 else
-        echo -e "${DIM}  ${GRN}[✓]${NC}" 
+        echo -e "  ${GRN}[√] done.${NC}\n"
 fi
 # CHECK IF BACKUP USER GROUP EXISTS
 echo -e "${PUR}• ${BLU}checking for bmbak group ${NC}\n"
@@ -32,17 +32,22 @@ if [ $? != 0 ]
 then
         echo -e "${RED}  [X] ${B}error     ${YLW}        bmbak is missing\n${NC}\n"
         # CREATE BMBAK GROUP
-        echo -e "${PUR}• ${BLU}        creating bmbak (1999) ${NC}\n"
+        echo -e "${PUR}• ${BLU}creating bmbak (1999) ${NC}\n"
         groupadd -g 1999 bmbak
-        getent group | grep bmbak
+        usermod -a -G bmbak ${USER}
+        usermod -a -G root ${USER}
+        echo -e "  ${GRN}[√] done.${NC}\n"
         # useradd -g $USERGROUP -d /home/$USERNAME -s /bin/bash -m $USERNAME
 fi
-
-# CREATE /NFS/HOST FOLDER
+echo -e "${PUR}• ${BLU}creating folder & fstab mount ${NC}\n"
+# CREATE /NFS/HOST | PERMISSIONS
 mkdir -p /nfs/${HOSTNAME}
-# ASSIGN PERMISSIONS
-
-# ADD FSTAB MOUNT
+chown -R bmilcs:bmbak /nfs/${HOSTNAME}
+chmod 770 /nfs/${HOSTNAME}
+echo -e "  ${GRN}[√] done.${NC}\n"# ADD FSTAB MOUNT
 FST='10.9.9.100:/mnt/bm/data/backup/'${HOSTNAME}'   /nfs/'$HOSTNAME'     nfs     auto,defaults,nofail 0 0'
 sudo grep -qxF "${FST}" /etc/fstab || sudo echo "${FST}" >> /etc/fstab
-
+# MOUNT FOLDER
+echo -e "${PUR}• ${BLU}attempting to mount nfs mount ${NC}\n"
+sudo mount -av
+echo -e "  ${GRN}[√] done.${NC}\n"
