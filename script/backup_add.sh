d89f6138e9a7632ce136daed0d4f57759e0df2c9
bmilcs <bmilcs@yahoo.com>
2020-10-30 19:10:24 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup_add.sh
+++ script/backup_add.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
-echo -e "${PUR}• ${BLU}add $1 to automated backup? ${NC}"
+echo -e "${PUR}• ${BLU}add $PWD to automated backup? ${NC}"
 read -p "      PROCEED? (y/n)  " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 				echo -e "${RED}  [X] ${B}error     ${YLW}cancelled request\n${NC}"
