383aa8f2e799b48db67eedf7eb98ce6cdfeef3c6
bmilcs <bmilcs@yahoo.com>
2020-10-30 19:01:50 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup_add.sh
+++ script/backup_add.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
 echo -e "${PUR}• ${BLU}add $1 to automated backup? ${NC}"
 read -p "      PROCEED? (y/n)  " -n 1 -r
 if [[ ! $REPLY =~ ^[Yy]$ ]]; then
@@ -6,10 +7,7 @@ if [[ ! $REPLY =~ ^[Yy]$ ]]; then
 				exit 0
 fi        
 
-dirname=${1}
-shopt -s extglob           # enable +(...) glob syntax
-result=${dirname%%+(/)}    # trim however many trailing slashes exist
-result=${result##*/}       # remove everything before the last / that still remains
-printf '%s\n' "$result"
-
 grep "$1" /etc/rsnapshot.conf || echo -e "backup\t${1}\t/nfs/${HOSTNAME}" >> /etc/rsnapshot.conf
+
+rsnapshot configtest
+
