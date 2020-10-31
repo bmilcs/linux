861a04055beb2e85e3dd89b524a3f52aa762b1f3
bmilcs <bmilcs@yahoo.com>
2020-10-31 17:57:17 -0400

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/update_sys.sh
+++ script/update_sys.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
+echo -e "${PUR}• ${BLU}sudo apt update -y ${NC}"
 sudo apt update -y
+echo -e "${PUR}• ${BLU}sudo apt full-upgrade -y ${NC}"
 sudo apt full-upgrade -y
+echo -e "${PUR}• ${BLU}sudo apt autoremove -y ${NC}"
 sudo apt autoremove -y
+echo -e "${PUR}• ${BLU}sudo apt clean -y ${NC}"
 sudo apt clean -y
+echo -e "${PUR}• ${BLU}sudo apt autoclean -y ${NC}"
 sudo apt autoclean -y
