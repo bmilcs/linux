2c73afaec79a727d2ffddcdadbe1801f89eb0b9d
bmilcs <bmilcs@yahoo.com>
2020-11-01 11:58:10 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/backup.sh
+++ script/backup.sh
@@ -6,7 +6,7 @@
 # INIT COLOR-VAR'S
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
 
-echo -e "\$1 is ${1}\n\$2 is ${2}"
+# echo -e "\$1 is ${1}\n\$2 is ${2}"   #debugging
 
 if [ "$1" = "add" ] ; then
         #PROMPT
