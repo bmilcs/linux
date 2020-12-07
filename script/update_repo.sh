7f42c1e8ad13cb87e8a511cde1c51c742c5d9949
bmilcs <bmilcs@yahoo.com>
2020-12-07 07:31:54 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/update_repo.sh
+++ script/update_repo.sh
@@ -3,8 +3,14 @@ NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5
 
 #===  update_repo.sh  =====================================================================#
 
-set -e
-echo -e "${BLU}[ ${B}github pull ${BLU}]${NC}\n"
-(cd ~ && rm -rf ~/.bm_* ~/.dir_colors ~/.bm ~/.bmilcs ~/_bmilcs && git clone https://github.com/bmilcs/linux.git ~/.bmilcs > /dev/null && chmod -R +x ~/.bmilcs && cd ~/.bmilcs/dotfiles > /dev/null ; ./install.sh > /dev/null && source ~/.bashrc > /dev/null)
-echo -e "  ${GRN}[√] done.${NC}\n"
+CHECK_PAC=$(which pacman)
+
+ if [[ ! -z $CHECK_PAC ]]; then
+	echo 'error: arch-based distro. ignoring bmilcs-linux github.'
+else
+	set -e
+	echo -e "${BLU}[ ${B}github pull ${BLU}]${NC}\n"
+	(cd ~ && rm -rf ~/.bm_* ~/.dir_colors ~/.bm ~/.bmilcs ~/_bmilcs && git clone https://github.com/bmilcs/linux.git ~/.bmilcs > /dev/null && chmod -R +x ~/.bmilcs && cd ~/.bmilcs/dotfiles > /dev/null ; ./install.sh > /dev/null && source ~/.bashrc > /dev/null)
+	echo -e "  ${GRN}[√] done.${NC}\n"
+fi
 # echo -e "${BLU}${DIM}----  ${BLU}bmilcs.git update: ${GRN}completed  ${BLU}${DIM}---------------------------------------------------------------\n${NC}"    
