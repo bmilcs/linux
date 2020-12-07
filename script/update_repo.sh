7d2e4a50103408bf7f8a291219b27af953fa2d19
bmilcs <bmilcs@yahoo.com>
2020-12-07 07:37:43 -0500

    a
    Signed-off-by: bmilcs <bmilcs@yahoo.com>

--- script/update_repo.sh
+++ script/update_repo.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
-
 #===  update_repo.sh  =====================================================================#
-
 CHECK_PAC=$(which pacman)
-
 if [[ ! -z $CHECK_PAC ]]; then
 	echo 'error: arch-based distro. ignoring bmilcs-linux github.'
 else
--- script/update_sys.sh
+++ script/update_sys.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 NC='\033[0m';B='\033[1m';DIM='\033[2m';ITAL='\033[3m';UL='\033[4m';BLINK='\033[5m';INV='\033[7m'; BLK=${NC}'\033[30m';RED=${NC}'\033[31m';GRN=${NC}'\033[32m';YLW=${NC}'\033[33m';BLU=${NC}'\033[34m';PUR=${NC}'\033[35m';CYN=${NC}'\033[36m';WHT=${NC}'\033[37m';TIME="$(date +"%I:%M %P")"
-set -e
+# set -e
 
 CHECK_PAC=$(which pacman)
 
- if [[ ! -z $CHECK_PAC ]]; then
+if [[ ! -z $CHECK_PAC ]]; then
 	echo -e "${BLU}[ ${B}sudo pacman -Syyuu${BLU}]${NC}"
 	sudo pacman -Syyuu
 	echo -e "${BLU}[ ${B}yay -Syyuu${BLU}]${NC}"
